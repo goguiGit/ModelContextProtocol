4b4747d8490fd10036ab08cc3305731aa13bac94
Rodrigo Valdez <rodrigo.valdez@visma.com>
2025-08-28 22:44:13 +0200

    add first server mcp

---
+++ src/ModelContextProtocol.MCP.ServerSDK/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+var server = builder.Services.AddMcpServer(options =>
+    {
+        options.ServerInfo = new ModelContextProtocol.Protocol.Implementation
+        {
+            Name = "My First MCP Server",
+            Version = "1.0"
+        };
+    })
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+var app = builder.Build();
+await app.RunAsync();
---
+++ src/ModelContextProtocol.MCP.ServerSDK/Tools/SumTools.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using ModelContextProtocol.Server;
+namespace ModelContextProtocol.MCP.ServerSDK.Tools;
+
+
+[McpServerToolType]
+public static class SumTools
+{
+    [McpServerTool, Description("Get sum of two numbers")]
+    public static string GetSum([Description("firstNumber")] int a, 
+        [Description("secondNumber")] int b)
+    {
+        return (a + b).ToString();
+    }
+}
+
